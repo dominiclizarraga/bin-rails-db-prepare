44bb7b7792be1020a0ecb431ea61a034db9f6242
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-11-19 21:59:02 -0800

    add testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a name" do
+    user = User.new(email: "johndoe@example.com")
+    assert_not user.valid?
+    user.name = "John Doe"
+    assert user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(name: "John", email: "")
+    assert_not @user.valid?
+    @user.email = "invalid"
+    assert_not @user.valid?
+    @user.email = "johndoe@example.com"
+    assert @user.valid?
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(
+    name: "John", email: "jd@example.com"
+    )
+    assert @existing_user.persisted?
+    @user = User.new(name: "Jon", email: "jd@example.com")
+    assert_not @user.valid?
+  end
 end
