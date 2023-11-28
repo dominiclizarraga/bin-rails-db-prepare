35f53c0c544e148517c6e3d823366614e070456f
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-11-27 16:41:18 -0800

    add route for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root "feed#show"
+
   get "sign_up", to: "users#new"
+
   post "sign_up", to: "users#create"
+
+  get "login", to: "sessions#new"
+
+  post "login", to: "sessions#create"
+  
 end
